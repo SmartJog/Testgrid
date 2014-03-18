4edf3cb11d02b60f4eb031d32320cf12024c24d9
florent claerhout <florent.claerhout@gmail.com>
2014-03-18 23:49:25 +0100

    Rename expected .ini name for local client.

--- testgrid/client/local.py
+++ testgrid/client/local.py
@@ -10,6 +10,6 @@ class Session(testgrid.server.model.Session):
 
 	def __init__(self):
 		super(Session, self).__init__(
-			grid = testgrid.server.parser.parse_grid("grid", "~/localgrid.ini"),
+			grid = testgrid.server.parser.parse_grid("grid", "~/grid.ini"),
 			subnet = None, # FIXME
 			key = None) # anonymous only as there is no controller process
