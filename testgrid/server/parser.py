76f87f70e3bcf56a02d7070df28c7e6ec9e8739e
florent claerhout <florent.claerhout@gmail.com>
2014-04-01 19:44:20 +0200

    Cleaning up parser.

--- testgrid/server/parser.py
+++ testgrid/server/parser.py
@@ -1,13 +1,28 @@
 # copyright (c) 2013-2014 smartjog, released under the GPL license.
 
+"""
+grid = parse_grid(name, path)
+
+Parse an .ini file and instanciate the corresponding grid object.
+
+Node objects:
+	[name]
+	type = ...
+	extra args...
+
+Grid objects:
+	[name]
+	nodes = ...
+	extra args...
+"""
+
 import ConfigParser, tempfile, unittest, textwrap, inspect, os
 
-import model, factory, debian, testbox
+import factory, model
 
 class ConfigurationError(Exception): pass
 
 class Parser(object):
-
 	"parse manifest of a grid and object creation"
 
 	def __init__(self, gridName, fileName, parentGrid=model.Grid, parentNode=model.Node):
@@ -23,8 +38,10 @@ class Parser(object):
 		self._toObject = {"nodes": self.generateNodes}
 
 	def createObjectFromSection(self, sectionName, parentSignature):
-		""" create an instantiated subclass object from a specific section of config file  
-		and parent object signature"""
+		"""
+		Create an instantiated subclass object from a specific
+		section of config file and parent object signature.
+		"""
 		if not self.config.has_section(sectionName):
 			raise ConfigurationError("%s: unknown section" % n)
 		objectType = self.config.get(sectionName, 'type')
@@ -41,7 +58,7 @@ class Parser(object):
 		return objectSignature(**arg)
 
 	def generateNodes(self, valueNodes):
-		"""generate list of instantiated node object """
+		"generate list of instantiated node object"
 		nodes = []
 		for n in valueNodes.split(" "):
 			nodes.append(self.createObjectFromSection(n, self.parentNode))	
@@ -51,21 +68,16 @@ class Parser(object):
 		"parse manifests and return a grid instance"
 		return self.createObjectFromSection(self.gridName, self.parentGrid)
 
-def parse_grid(name, ini):
+def parse_grid(name, path):
 	"parse manifests and return a grid instance"
-	parser = Parser(name, ini)
+	parser = Parser(name, path)
 	return parser.parse()
 
 ##############
 # unit tests #
 ##############
 
-class parserFakeGrid(model.Grid):
-
-	def __init__(self, nodes):
-		super(parserFakeGrid, self).__init__(nodes)
-
-class  testArg(model.Grid):
+class testArg(model.Grid):
 
 	def __init__(self, required, optional="opt", optional2="opt2"):
 		super(testArg, self).__init__()
@@ -75,7 +87,8 @@ class  testArg(model.Grid):
 class SelfTest(unittest.TestCase):
 
 	@staticmethod
-	def file(content = ""):
+	def get_file(content = ""):
+		"generate a temporary file with the specified content"
 		f = tempfile.NamedTemporaryFile()
 		f.write(textwrap.dedent(content))
 		f.flush()
@@ -83,12 +96,12 @@ class SelfTest(unittest.TestCase):
 
 	def test_grid_not_found(self):
 		"assert parse_grid raises an exception on a missing section"
-		f = self.file()
+		f = self.get_file()
 		self.assertRaises(Exception, parse_grid, "foo", f.name)
 
 	def test_basic_grid(self):
 		"assert parse_grid return a model.Grid if type is grid"
-		f = self.file("""
+		f = self.get_file("""
 			[foo]
 			type = grid
 		""")
@@ -97,33 +110,32 @@ class SelfTest(unittest.TestCase):
 
 	def test_basic_grid_with_nodes(self):
 		""" assert parse_grid return a parserFakeGrid instantiated with debian Node"""
-		f = self.file("""
+		f = self.get_file("""
 			[node1]
-			type = debian Node
+			type = fake Node
 			hoststring = root@a.b.c.d
 
 			[node2]
-			type = Debian Node
+			type = Fake Node
 			hoststring = root@a.b.c.d
 
 			[node3]
-			type = debiAn node
+			type = FaKe NoDe
 			hoststring = root@a.b.c.d
 
 			[foo]
-			type = parserFakeGrid
+			type = grid
 			nodes = node1 node2 node3
-
 		""")
 		grid = parse_grid("foo", f.name)
-		assert type(grid) is parserFakeGrid
+		assert type(grid) is model.Grid
 		assert len(grid.nodes) is 3
 		for n in grid.nodes:
-			assert type(n) is debian.Node
+			assert type(n) is model.FakeNode
 
 	def test_testbox_grid(self):
 		"assert parse_grid return a testbox.Grid if type is testbox grid"
-		f = self.file("""
+		f = self.get_file("""
 			[foo]
 			type = testbox grid
 			entry_path = ./en0wifi
@@ -133,7 +145,7 @@ class SelfTest(unittest.TestCase):
 
 	def test_option_not_found(self):
 		"assert parse_grid raises an exception on a wrong option"
-		f = self.file("""
+		f = self.get_file("""
 			[foo]
 			type = grid
 			bad_arg = bad_arg
@@ -141,7 +153,7 @@ class SelfTest(unittest.TestCase):
 		self.assertRaises(Exception, parse_grid, "foo", f.name)
 
 	def test_required_arg(self):
-		f = self.file("""
+		f = self.get_file("""
 			[foo]
 			type = testArg
 			required = required
