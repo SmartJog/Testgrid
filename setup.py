3aabbfc635abf942d3cf1e6b60608d1c5b25457d
florent claerhout <florent.claerhout@gmail.com>
2014-04-03 12:16:28 +0200

    Idem.

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 # copyright (c) 2013-2014 smartjog, released under the GPL license.
 
 import setuptools
-import testgrid.main
 
 setuptools.setup(
 	name = "testgrid",
-	version = main.__version__,
+	version = "0.1",
 	author = "arkena",
 	author_email = "qa@tdf-ms.com",
 	description = "Programmable test environments framework and command-line utility",
