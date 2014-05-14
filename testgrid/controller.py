99056d6a940aad427b3c6d9aec1579e321e421bc
sbideaux <sofia.bideaux@smartjog.com>
2014-05-14 18:01:39 +0200

    continue rest api

--- testgrid/controller.py
+++ testgrid/controller.py
@@ -10,10 +10,23 @@ import parser
 import sys
 import model
 import debian
+import testgrid
 app = Bottle()
 grid = None
 
-class DebianPackage(debian.Package): pass
+def get_session(username, name):
+    for session in grid.get_sessions():
+        if (session.username == username and session.name == name):
+            return session
+    raise Exception("session %s %s doesn't exist" % (username, name))
+
+@app.route("/get_node")
+def get_node():
+    name = request.GET.get("name")
+    for node in grid:
+        if node.name == name:
+            return {"name": node.name ,"typename": node.get_typename(), "hoststring": node.hoststring}
+    return ({"error": "node %s doesn' exist" % name})
 
 @app.route("/get_nodes")
 def get_nodes():
@@ -46,48 +59,106 @@ def is_quarantined():
             return ({"result": grid.is_quarantined()})
     return ({"error": "node %s doesn' exist" % name})
 
-@app.route("/deploy")
+@app.post("/deploy")
 def deploy():
     try:
-        packages = request.GET.getall("pkg")
+        data = request.json
         list_packages = []
-        for value in packages:
-            pkg, pkg_type = value.partition(" ")[::2]
-            pkg_name, pkg_version = pkg.partition("=")[::2]
-            #parser package type
-            list_packages.append(debian.Package(name = pkg_name, version = pkg_version))
-        session = request.GET.get("session")
-        username, name = session.partition(" ")[::2]
-        session_obj = grid.open_session(username, name)
-        plans = session_obj.deploy(list_packages)
+        for package in data["packages"]:
+            cls = parser.get_subclass(package["type"], testgrid.model.Package ,package["module"])
+            list_packages.append(cls(name = package["name"], version = package["version"]))
+        session = get_session(data["session"]["username"], data["session"]["name"])
+        plans = session.deploy(list_packages)
         result = {}
         for pkg, node in plans:
-            result[node.name] = {"pkg":(pkg.name, pkg.version), "args": {"typename": node.get_typename(), "hoststring": node.hoststring}}
+            result[node.name] = {"pkg":{"name": pkg.name, "version": pkg.version}, "args": {"typename": node.get_typename(), "hoststring": node.hoststring}}
         return result
     except Exception as e:
         return {"error": repr(e)}
 
-@app.route("/undeploy")
+@app.post("/undeploy")
 def undeploy():
-    session = request.GET.get("session")
-    username, name = session.partition(" ")[::2]
-    session_obj = grid.open_session(username, name)
-    session_obj.undeploy()
-    return {"result": True}
+    try:
+        data = request.json
+        session = get_session(data["session"]["username"], data["session"]["name"])
+        session.undeploy()
+        return {}
+    except Exception as e:
+        return {"error": repr(e)}
 
-@app.route("/open_session")
+@app.post("/open_session")
 def open_session():
-    username = request.GET.get("username")
-    name = request.GET.get("name")
-    session = grid.open_session(username, name)
-    return {"result": True}
+    try:
+        data = request.json
+        session = grid.open_session(data["session"]["username"], data["session"]["name"])
+        return {}
+    except Exception as e:
+        return {"error": repr(e)}
 
-@app.route("/allocate_node")
+@app.post("/allocate_node")
 def allocate_node():
-    session = request.GET.get("session")
-    username, name = session.partition(" ")[::2]
-    session_obj = grid.open_session(username, name)
-    print request.GET.items()
+    try:
+        data = request.json
+        session = get_session(data["session"]["username"], data["session"]["name"])
+        node = session.allocate_node(**data["options"])
+        return {"name": node.name,"typename": node.get_typename(), "hoststring": node.hoststring}
+    except Exception as e:
+        return {"error": repr(e)}
+
+@app.post("/release_node")
+def release_node():
+    try:
+        data = request.json
+        session = get_session(data["session"]["username"], data["session"]["name"])
+        for node in grid:
+            print data["node"]["name"], node.name
+            if node.name == data["node"]["name"]:
+                session.release_node(node)
+                print session.plan
+                return {}
+        return ({"error": "node %s doesn' exist" % data["node"]["name"]})
+    except Exception as e:
+        return {"error": repr(e)}
+
+@app.route('/get_node_session')
+def get_node_session():
+    name = request.GET.get("name")
+    for session in grid.get_sessions():
+        for node in session:
+            if node.name == name:
+                return {"session": {"username": session.username, "name": session.name}}
+    return {}
+
+@app.post('/close_session')
+def close_session():
+    try:
+        data = request.json
+        session = get_session(data["session"]["username"], data["session"]["name"])
+        session.close()
+        return {}
+    except Exception as e:
+        return {"error": repr(e)}
+
+@app.route('/get_sessions')
+def get_sessions():
+    sessions = {"sessions": []}
+    for session in grid.get_sessions():
+        sessions["sessions"].append({"username": session.username, "name": session.name})
+    return sessions
+
+@app.route('/get_session')
+def get_session_rest():
+    try:
+        name = request.GET.get("name")
+        uername = request.GET.get("username")
+        session = get_session(username, name)
+        result = {}
+        for node in session:
+            result[node.name] = {"typename": node.get_typename(), "hoststring": node.hoststring}
+        return result
+    except Exception as e:
+        return {"error": repr(e)}
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 4:
--- testgrid/rest.py
+++ testgrid/rest.py
@@ -2,6 +2,7 @@
 import requests
 import testgrid
 import getpass
+import json
 
 class Node(testgrid.model.Node):
 
@@ -30,36 +31,59 @@ class Node(testgrid.model.Node):
         def get_installed_packages(self):pass
 
 class Session(object):
+
         def __init__(self, host, username, name = None):
                 self.host = host
                 self.username = username
                 self.name = name
 
         def deploy(self, packages):
-                url = 'http://%s/deploy?' % self.host
+                url = 'http://%s/deploy' % self.host
+                data = {}
+                list_packages = []
                 for pkg in packages:
-                        url += "pkg=%s=%s+%s&" % (pkg.name, pkg.version, type(pkg).__name__)
-                url+= "session=%s+%s" % (self.username, self.name)
-                r = requests.get(url)
-                data = r.json()
-                if "error" in data:
-                        raise Exception(data["error"])
+                        list_packages.append({"name": pkg.name, "version": pkg.version, "type": type(pkg).__name__, "module": type(pkg).__module__})
+                data["packages"] = list_packages
+                data["session"] = {"username":self.username, "name": self.name}
+                headers = {'content-type': 'application/json'}
+                r = requests.post(url, data=json.dumps(data), headers=headers)
+                response = r.json()
+                if "error" in response:
+                        raise Exception(response["error"])
                 plans = []
-                for key, value in data.items():
-                        plans.append((value["pkg"], Node(host = self.host, name = key, **value["args"])))
+                for key, value in response.items():
+                        plans.append((value["pkg"]["name"] , Node(host = self.host, name = key, **value["args"])))
                 return plans
 
         def undeploy(self):
-                url = 'http://%s/undeploy?session=%s+%s' % (self.host, self.username, self.name)
-                r = requests.get(url)
+                url = 'http://%s/undeploy' % self.host
+                data = {"session" :{"username":self.username, "name": self.name}}
+                headers = {'content-type': 'application/json'}
+                r = requests.post(url, data=json.dumps(data), headers=headers)
+                response = r.json()
+                if "error" in response:
+                        raise Exception(response["error"])
 
         def allocate_node(self, **opts):
-                url = 'http://%s/allocate_node?'
-                for key, value in opts:
-                        url += "%s=%s&" % (key, value)
-                r = requests.get(url)
+                url = 'http://%s/allocate_node' % self.host
+                data = {"session" :{"username":self.username, "name": self.name}}
+                data["options"] = opts
+                headers = {'content-type': 'application/json'}
+                r = requests.post(url, data=json.dumps(data), headers=headers)
+                response = r.json()
+                if "error" in response:
+                        raise Exception(response["error"])
+                return Node(self.host, **response)
 
-        def release_node(node):pass
+        def release_node(self, node):
+                url = 'http://%s/release_node' % self.host
+                data = {"session" :{"username":self.username, "name": self.name}}
+                data["node"] = {"name": node.name}
+                headers = {'content-type': 'application/json'}
+                r = requests.post(url, data=json.dumps(data), headers=headers)
+                response = r.json()
+                if "error" in response:
+                        raise Exception(response["error"])
 
 class Client(testgrid.client.Client):
 
@@ -67,14 +91,21 @@ class Client(testgrid.client.Client):
                 self.host = host
                 self.username = username or getpass.getuser()
 
+        def get_node(self, name):
+                url = 'http://%s/get_node?name=%s' % (self.host, name)
+                r = requests.get(url)
+                response = r.json()
+                if "error" in response:
+                        raise Exception(response["error"])
+                return Node(self.host, **response)
+
         def get_nodes(self):
                 url = 'http://%s/get_nodes' % self.host
                 r = requests.get(url)
-                if r.status_code == 200:
-                        data = r.json()
-                        for key, value in data.items():
-                                yield Node(host = self.host, name = key, **value)
-                #return r.raise_for_status()
+                r.raise_for_status()
+                data = r.json()
+                for key, value in data.items():
+                        yield Node(host = self.host, name = key, **value)
 
         def is_available(self, node):
                 url = 'http://%s/is_available?name=%s' % (self.host, node.name)
@@ -97,11 +128,74 @@ class Client(testgrid.client.Client):
                         data = r.json()
                         return data["result"]
 
+        def is_transient(self, node):pass
+
         def get_node_session(self, node):
-                return None
-                #url = 'http://%s/get_node_session?name=%s' % (self.host, node.name)
+                url = 'http://%s/get_node_session?name=%s' % (self.host, node.name)
+                r = requests.get(url)
+                data = r.json()
+                if "session" in data:
+                        return Session(self.host, data["session"]["username"], data["session"]["name"])
 
         def open_session(self, name = None):
-                url = 'http://%s/open_session?username=%s&name=%s' % (self.host, self.username, name)
-                r = requests.get(url)
+                url = 'http://%s/open_session' % self.host
+                data = {}
+                data["session"] = {"username": self.username, "name": name}
+                headers = {'content-type': 'application/json'}
+                r = requests.post(url, data=json.dumps(data), headers=headers)
+                response = r.json()
+                if "error" in response:
+                        raise Exception(response["error"])
                 return Session(self.host, self.username, name)
+
+        def close_session(self, name):
+                url = 'http://%s/close_session' % self.host
+                data = {"session" :{"username":self.username, "name": name}}
+                headers = {'content-type': 'application/json'}
+                r = requests.post(url, data=json.dumps(data), headers=headers)
+                response = r.json()
+                if "error" in response:
+                        raise Exception(response["error"])
+
+        def get_session(self, name):
+                url = 'http://%s/get_session?name=%s&username=%s' % (self.host, name, self.username)
+                r = requests.get(url)
+                response = r.json()
+                if "error" in response:
+                        raise Exception(response["error"])
+                return Session(self.host, self.username, name)
+
+        def get_sessions(self):
+                url = 'http://%s/get_sessions' % self.host
+                r = requests.get(url)
+                response = r.json()
+                sessions = {}
+                for session in response["sessions"]:
+                        yield Session(self.host, session["username"], session["name"])
+
+
+
+        def add_node(self, name, ini):
+                url = 'http://%s/add_node' % self.host
+                dic = self.get_node_dictionary(name, ini)
+                headers = {'content-type': 'application/json'}
+                r = requests.post(url, data=json.dumps(dic), headers=headers)
+                response = r.json()
+                if "error" in response:
+                        raise Exception(response["error"])
+
+        def remove_node(self, name):
+                url = 'http://%s/remove_node' % self.host
+
+        def quarantine_node(self, name, reason):
+                url = 'http://%s/quarantine_node' % self.host
+
+        def rehabilitate_node(self, name):
+                url = 'http://%s/rehabilitate_node' % self.host
+
+
+
+
+
+
+
