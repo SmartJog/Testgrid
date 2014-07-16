b6b53bfb37de45e65dc0a911c79d89bc85531bde
Florent Claerhout <florent.claerhout@smartjog.com>
2014-07-16 11:22:05 +0200

    work on test.py

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
-from testgrid.model import *
-from testgrid.parser import *
-from testgrid.client import *
-from testgrid.controller import *
-from testgrid.rest import *
-from testgrid.isadapter import *
-from testgrid.persistent import *
-from testgrid.database import *
 
 import unittest
 
-if __name__  == "__main__": unittest.main(verbosity = 2)
+import testgrid
+
+testmodules = [
+	'testgrid.model',
+	'testgrid.database',
+	'testgrid.controller',
+	# to be completed...
+]
+
+suite = unittest.TestSuite()
+
+for t in testmodules:
+	suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
+
+if __name__  == "__main__": unittest.TextTestRunner(verbosity = 2).run(suite)
+
