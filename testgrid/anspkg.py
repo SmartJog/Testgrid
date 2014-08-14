505df6e922d8e62cc98565f04d439ebf7d14a9c7
sbideaux <sofia.bideaux@smartjog.com>
2014-08-14 18:11:52 +0200

    working on ansible playbook

--- testgrid/anspkg.py
+++ testgrid/anspkg.py
@@ -1,10 +1,17 @@
 # copyright (c) 2014 smartjog, released under the GPL license.
 
-import ansible
+# copyright (c) 2014 smartjog, released under the GPL license.                                                                                          
+
+import ansible.playbook
+from ansible import callbacks
+from ansible import utils
 
 class Playbook():
-    def __init__(self):
-        self.playbook = ansible.PlayBook( playbook_path = None,inventory = None)
+    def __init__(self, playbook_path = None, inventory = None,  callbacks = callbacks.PlaybookCallbacks(verbose=utils.VERBOSITY), runner_callbacks = ca\
+llbacks.PlaybookRunnerCallbacks(callbacks.AggregateStats(), verbose=utils.VERBOSITY), stats = callbacks.AggregateStats()):
 
-    def run():
+        self.playbook = ansible.playbook.PlayBook(playbook = playbook_path,inventory = inventory, callbacks = callbacks, runner_callbacks = runner_call\
+backs, stats = stats)
+
+    def run(self):
         return self.playbook.run()
